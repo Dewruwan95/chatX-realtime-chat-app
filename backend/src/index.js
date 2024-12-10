4c6e5dbfc7c6943bc68cacd8afdcb923b41a98e9
Ositha Dewruwan <dewruwanebay@gmail.com>
2024-12-10 19:45:27 +0530

    created message model and message routes with controller functionalities

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import { connectDB } from "./lib/db.js";
 import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.route.js";
 import cookieParser from "cookie-parser";
 
 dotenv.config();
@@ -14,6 +15,7 @@ app.use(cookieParser());
 const PORT = process.env.PORT || 5001;
 
 app.use("/api/auth", authRoutes);
+app.use("/api/message", messageRoutes);
 
 app.listen(PORT, () => {
   console.log("Server is running on PORT " + PORT);
