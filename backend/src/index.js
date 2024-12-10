9bbf90708caca61daa89265932069f10cac4eebe
Ositha Dewruwan <dewruwanebay@gmail.com>
2024-12-10 18:41:38 +0530

    completed auth route functionalities

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,12 +2,14 @@ import express from "express";
 import dotenv from "dotenv";
 import { connectDB } from "./lib/db.js";
 import authRoutes from "./routes/auth.route.js";
+import cookieParser from "cookie-parser";
 
 dotenv.config();
 
 const app = express();
 
 app.use(express.json()); // middleware for extrate data as json
+app.use(cookieParser());
 
 const PORT = process.env.PORT || 5001;
 
